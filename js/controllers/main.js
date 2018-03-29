0355678efa5d0a5521c469fb60360190f8a7f373
prakash7755 <mailmeprakash1992@gmail.com>
2018-03-29 20:05:17 +0530

    Register Form

---
+++ js/controllers/main.js
@@ -0,0 +1,19 @@
+'use strict';
+(function () {
+	const app = angular.module('myApp');
+
+	app.controller('MyCtrl',[
+		 MyCtrl
+		]);
+
+	function MyCtrl(){
+		const vm = this;
+
+		vm.RegisterMe = function(data){
+		console.log('WELCOME');
+
+		}
+	}
+
+
+})()
