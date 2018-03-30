4e76cb80a96b78b8223ac6086da96f01064b070e
prakash7755 <mailmeprakash1992@gmail.com>
2018-03-30 16:30:01 +0530

    ngRouter

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 'use strict';
 (function () {
-	const app = angular.module('myApp', []);
+	const app = angular.module('myApp', ['ngRoute']);
+	app.config(function (){
+		$routeProvider
+		.when('/login',{
+			template: "<h2>login STATE</h2>"
+		})
+		.when('/Register',{
+			template: "<h2>Register STATE</h2>"
+		})
+		// console.log('jdshfuidshvf');
+	})
 })();
